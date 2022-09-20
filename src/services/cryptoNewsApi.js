7fa990dc780a6ed5dc0bbb8f7765af7288741e80
Moustapha LO <lmoustapha2@gmail.com>
2022-09-20 11:49:23 +0000

    First deployed

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '13c964c50bmsh35a32bab7e7d2e1p169115jsn779ee1c4f297',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoNewsHeaders});
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
